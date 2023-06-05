b9f3af3d7991de5de5cc1530d0fee3c14e220e16
CharlieMCY <cyma@stu.xidian.edu.cn>
2023-06-05 20:02:55 +0800

    modify: config.mk

--- config.mk
+++ config.mk
@@ -1,3 +1,3 @@
-export CROSS_COMPILE := /home/optee/extdisk_1/optee/rpi3/toolchains/aarch32/bin/arm-linux-gnueabihf-
-export CROSS_COMPILE_AARCH64 := /home/optee/extdisk_1/optee/rpi3/toolchains/aarch64/bin/aarch64-linux-gnu-
+export CROSS_COMPILE :=
+export CROSS_COMPILE_AARCH64 :=
 
